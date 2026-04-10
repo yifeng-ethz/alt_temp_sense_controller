6d70dc446251625204f721880cb0400029a57587
Yifeng Wang <yifenwan@phys.ethz.ch>
2026-04-10 11:40:15 +0200

    Bump altera_temp_sense_ctrl to v1.1

--- altera_temp_sense_ctrl_hw.tcl
+++ altera_temp_sense_ctrl_hw.tcl
@@ -20,7 +20,7 @@ package require -exact qsys 16.1
 # 
 set_module_property DESCRIPTION ""
 set_module_property NAME altera_temp_sense_ctrl
-set_module_property VERSION 1.0
+set_module_property VERSION 1.1
 set_module_property INTERNAL false
 set_module_property OPAQUE_ADDRESS_MAP true
 set_module_property GROUP "Mu3e Control Plane/Modules"
